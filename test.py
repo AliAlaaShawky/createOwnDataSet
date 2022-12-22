71037788e756f8e9e0159e07fc97c66501f55e0d
ali alaa <86577146+alialaabos@users.noreply.github.com>
2022-12-22 17:50:50 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-cam=cv2.VideoCapture("C:/Users/xps/Downloads/ahmed shaban.mp4")
+cam=cv2.VideoCapture(0) # you can replace 0 with vedio path
 detector=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 sampleNum=0
 while (True):
@@ -8,7 +8,7 @@ while (True):
     faces=detector.detectMultiScale(img,1.3,5)
     for(x,y,w,h) in faces:
         sampleNum=sampleNum+1
-        cv2.imwrite("fainal data set/ahmed shaban/User."+str(sampleNum)+".jpg",img[y:y+h,x:x+w])
+        cv2.imwrite("data set//User."+str(sampleNum)+".jpg",img[y:y+h,x:x+w])
         cv2.imshow('frame',img)
         #sendMsg(msg)
 
